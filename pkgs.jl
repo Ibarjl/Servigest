1f7493cc6bad910cd750cef15375873ba594d9d8
Ibar <ibarvivas@gmail.com>
2025-07-14 12:49:31 +0200

    anexo de html

---
+++ pkgs.jl
@@ -0,0 +1,6 @@
+using Pkg
+Pkg.add("Genie")
+Pkg.add("LibPQ") # Para interactuar con PostgreSQL
+Pkg.add("SearchLight") # ORM para Genie, opcional pero útil
+Pkg.add("Stipple") # Para interactividad reactiva, si la necesitas
+Pkg.add("StippleUI") # Componentes UI para Stipple
